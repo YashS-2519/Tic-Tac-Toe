def761bd58533daf349516aed5d22c3a4b3ac0d1
YashS-2519 <y.singh2519@gmail.com>
2024-06-17 00:40:06 +0530

    Finalize Tic Tac Toe game

---
+++ readme.md
@@ -0,0 +1,63 @@
+# [Tic Tac Toe Game]()
+
+This is a simple Tic Tac Toe game implemented using HTML, CSS, and JavaScript. It allows two players to play the game in a browser.
+
+## Table of Contents
+
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshots](#screenshots)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- Classic Tic Tac Toe game with a 3x3 grid.
+- Two-player functionality.
+- Displays the winner or indicates a draw.
+- Responsive design for different screen sizes.
+
+## Installation
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/YashS-2519/Tic-Tac-Toe.git
+    ```
+
+2. Navigate to the project directory:
+    ```bash
+    cd tic-tac-toe
+    ```
+
+3. Open `index.html` in your browser to play the game.
+
+## Usage
+
+1. Open the `index.html` file in a web browser.
+2. The game starts with Player A. Click on any empty cell in the grid to make a move.
+3. Players take turns clicking on empty cells to place their marks (X or O).
+4. The game will display a message indicating the winner or if the game ends in a draw.
+5. Click the "Restart" button to reset the game and play again.
+
+## Screenshots
+
+![Game Screenshot](screenshots/img1.png)
+![Game Screenshot](screenshots/img2.png)
+![Game Screenshot](screenshots/img3.png)
+
+## Contributing
+
+Contributions are welcome! Please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature-branch`).
+3. Make your changes.
+4. Commit your changes (`git commit -m 'Add some feature'`).
+5. Push to the branch (`git push origin feature-branch`).
+6. Open a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
---
+++ script/script.js
@@ -0,0 +1,114 @@
+const cellArray = document.querySelectorAll('.cell');
+const button = document.querySelector('#start');
+
+const winPattern = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+]
+
+let occupiedCells = [];
+
+let playerA = [];
+let playerB = [];
+let currentPlayer = playerA;
+
+let alreadyPlay = 0;
+
+function isCellEmpty(cell) {
+    return !occupiedCells.includes(cell);
+}
+
+function isWin(player) {
+
+    let winStreak = 0;
+    player.sort();
+
+    for (const key in winPattern) {
+        winPattern[key].forEach( (elem) => {
+
+            if (player.includes(elem))
+                winStreak++;
+        })
+
+        if (winStreak === 3)
+            return true;
+
+        winStreak = 0;
+    }
+
+    return false;
+}
+
+function insertValue(currentPlayer, cellValue, cell) {
+    currentPlayer.push(cellValue);
+    occupiedCells.push(cellValue);
+    if (currentPlayer === playerA) {
+        cell.querySelector('h6').style.color = "#d32525";
+    }
+    else{
+        cell.querySelector('h6').style.color = "green";
+    }
+}
+
+function gameover(currentPlayer, draw) {
+    if (!draw) {
+        document.querySelector('.modal').style.display = 'flex';
+        document.querySelector('p').innerHTML = `Player ${currentPlayer === playerA ? 'A' : 'B'} wins!`;
+        document.querySelector('button').innerHTML = `Play again`;
+        alreadyPlay++;
+    }
+    else{
+        document.querySelector('.modal').style.display = 'flex';
+        document.querySelector('p').innerHTML = `It's a draw!`;
+        document.querySelector('button').innerHTML = `Play again`;
+        alreadyPlay++;
+    }
+}
+
+function gamestart() {
+    cellArray.forEach( (cell) => {
+        cell.addEventListener('click', () => {
+
+            let cellValue = parseInt(cell.getAttribute('value'))
+            if (isCellEmpty(cellValue)) {
+                insertValue(currentPlayer, cellValue, cell)
+                document.querySelector(`#${cell.id} h6`).innerHTML = currentPlayer === playerA ? 'X' : 'O';
+                if (isWin(currentPlayer)){
+                    gameover(currentPlayer, false)
+                    return 0;
+                }
+                currentPlayer = currentPlayer === playerA ? playerB : playerA;
+                document.querySelector('h5').innerHTML = `Player ${currentPlayer === playerA ? 'A' : 'B'}'s turn`;
+
+            }
+
+            if (occupiedCells.length == 9 && !isWin(currentPlayer)) {
+                gameover(0, 1);
+            }
+            console.log("playerA: " + playerA + " playerB: "+ playerB + "Occupied Cells" + occupiedCells.length);
+    
+        })
+    })
+}
+
+button.addEventListener('click', () =>{
+    if (alreadyPlay != 0) {
+        occupiedCells = [];
+        playerA = [];
+        playerB = [];
+        currentPlayer = playerA;
+        cellArray.forEach(cell => {
+            cell.querySelector('h6').innerHTML = '';
+            cell.querySelector('h6').style.color = '';
+        });
+    }
+    document.querySelector('.modal').style.display = 'none';
+    document.querySelector('h5').innerHTML = `Player ${currentPlayer === playerA ? 'A' : 'B'}'s turn`;
+    gamestart();
+})
